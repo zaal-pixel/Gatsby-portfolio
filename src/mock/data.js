09bef96851c06b1754de65c18988f1dea67369aa
alza <alzadinaya@gmail.com>
2020-10-20 20:09:13 +0200

    first

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,39 +1,40 @@
-import { nanoid } from 'nanoid';
+import {
+  nanoid
+} from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Alza', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my share of the Internet', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi I am Alza a',
+  name: 'Frontend Developer',
+  subtitle: 'Welcome to my share of the Internet',
+  cta: 'learn more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: '',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
-export const projectsData = [
-  {
+export const projectsData = [{
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'currencytrackepic.PNG',
+    title: 'Cryptotracker',
+    info: ' I used an API to create a simpe Onepage Cryptotracker',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://zaal-pixel.github.io/crypto-currency-tracker/',
+    repo: 'https://github.com/zaal-pixel/crypto-currency-tracker', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -57,33 +58,27 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'If you have any questions, dont hesitate to email me :D',
+  btn: 'email me',
+  email: 'alzadinaya@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
-  networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
+  networks: [{
       id: nanoid(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/zaal-pixel',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/alza-ahmad-dinaya-1aa941171/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/zaal-pixel',
     },
   ],
 };
